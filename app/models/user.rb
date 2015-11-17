7d2a29c7906c9edbf0825396d1b47b47a8960462
pedroassis <pedroassisc@gmail.com>
2015-11-17 20:57:30 -0200

    Atualizar schema.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,3 +5,23 @@ class User < ActiveRecord::Base
   validates :uid, presence: true, if: 'provider.present?'
   validates :password, length: {minimum: 4, maximum: 16}, allow_blank: true
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string(255)
+#  name            :string(255)
+#  provider        :string(255)
+#  uid             :string(255)
+#  password_digest :string(255)
+#  last_access_ip  :string(255)
+#  last_access_at  :datetime
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103000452) do
+ActiveRecord::Schema.define(version: 20151117222432) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -83,5 +83,19 @@ ActiveRecord::Schema.define(version: 20151103000452) do
   end
 
   add_index "questions", ["category_id"], name: "index_questions_on_category_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "password_digest"
+    t.string   "last_access_ip"
+    t.datetime "last_access_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
