71a56a0bbc3c2d6b2f8021982d50ee7e4cb00641
pedroassis <pedroassisc@gmail.com>
2015-11-18 17:40:24 -0200

    Layout para tela inicial.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  # Define o título da página
+  def title(page_title, show_title = true)
+    @page_title = page_title
+    @show_title = show_title
+  end
+
+  # Define o subtítulo da página
+  def subtitle(page_subtitle)
+    @page_subtitle = page_subtitle
+  end
+
+  def head_title
+    head_title = t('labels.application_title')
+
+    unless @page_title.nil?
+      head_title = "#{@page_title} - " + head_title
+    end
+
+    head_title
+  end
 end
