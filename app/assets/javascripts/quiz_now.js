f15ccf1eabf08d44c6bff0b4983e6c0853ab7d12
pedroassis <pedroassisc@gmail.com>
2015-11-18 18:29:59 -0200

    Marcar campos com erros em formulários.

--- app/assets/javascripts/quiz_now.js
+++ app/assets/javascripts/quiz_now.js
@@ -1,3 +1,5 @@
 $(function() {
     $('.alert-auto-hide').parent('.alert-container').fadeOut(15000)
+    $('.field_with_errors').parent('.form-group').addClass('has-error')
+    $('.has-error:first').find('input').focus()
 })
