9807935bb34c5ddb41b621ee40de0992c3311fe5
pedroassis <pedroassisc@gmail.com>
2015-11-18 19:10:24 -0200

    Ativar tooltips do bootstrap.

--- app/assets/javascripts/quiz_now.js
+++ app/assets/javascripts/quiz_now.js
@@ -2,4 +2,5 @@ $(function() {
     $('.alert-auto-hide').parent('.alert-container').fadeOut(15000)
     $('.field_with_errors').parent('.form-group').addClass('has-error')
     $('.has-error:first').find('input').focus()
+    $('a[data-toggle=tooltip]').tooltip({ animation: false })
 })
