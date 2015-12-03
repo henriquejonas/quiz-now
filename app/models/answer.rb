99ec15ca04d6b753216ab1fd99d1e1ece10c336e
Henrique Jonas <henrique@2x3.com.br>
2015-12-02 23:00:47 -0200

    Adicionando migration de questão correta

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -25,13 +25,13 @@ end
 #  answer             :text
 #  question_id        :integer
 #  order              :integer
-#  correct            :boolean
 #  created_at         :datetime
 #  updated_at         :datetime
 #  image_file_name    :string(255)
 #  image_content_type :string(255)
 #  image_file_size    :integer
 #  image_updated_at   :datetime
+#  correct            :boolean
 #
 # Indexes
 #
